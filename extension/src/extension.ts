63bcc22917375520f055f9b385178238f228d96d
Paul Jolly <paul@myitcv.io>
2024-09-09 13:26:47 +0100

    extension: minimal starting of cuepls
    DO NOT REVIEW
    DO NOT SUBMIT
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I8038acdb9e826631bcc741869ae679eb2fc95694

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,6 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { LanguageClient, ServerOptions } from 'vscode-languageclient/node';
+import { LanguageClientOptions } from 'vscode-languageclient';
+import { workspace, ExtensionContext } from 'vscode';
+
+let client: LanguageClient;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -19,6 +24,24 @@ export function activate(context: vscode.ExtensionContext) {
   });
 
   context.subscriptions.push(disposable);
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    command: '/home/myitcv/bingo/bin/cuepls'
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: 'file', language: 'cue' }]
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions);
+
+  // Start the client. This will also launch the server
+  client.start();
 }
 
 // This method is called when your extension is deactivated

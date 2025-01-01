6ccb22055226e16e8eafaa088ebb83d15f7995b8
Paul Jolly <paul@myitcv.io>
2025-01-01 16:14:57 +0000

    extension: use CUE as the source of truth for TypeScript
    Currently we rely on keeping the TypeScript source and CUE in sync
    manually. This is obviously brittle.
    
    As a step in the right direction, generated a TypeScript file that
    contains an exported version of the CUE configuration for the extension
    (i.e. data, not schema). We do this via the new genTS command.
    
    In order to be able to use the command IDs from TypeScript, we factor
    out the list of commands required by the VSCode Extension schema into a
    map, keyed by ID, and template the VSCode form from that map. We also
    factor out the command prefix "CUE: " and modify the
    Extension.registerCommand method to use the new config struct.
    
    We also provide an Extension.commandID helper method to construct a
    command ID from a command name. This is used again in a later CL.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Ifd6c76f101025da242edabb69049a7bbf7090a20

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -20,6 +20,7 @@ import * as vscode from 'vscode';
 import * as lcnode from 'vscode-languageclient/node';
 import * as cp from 'node:child_process';
 import * as lc from 'vscode-languageclient';
+import { config } from './gen_userCommands';
 
 let errTornDown = new Error('Extenssion instance already torn down');
 
@@ -92,9 +93,9 @@ export class Extension {
 		let configChangeListener = vscode.workspace.onDidChangeConfiguration(this.extensionConfigurationChange);
 		this.ctx.subscriptions.push(configChangeListener);
 
-		this.registerCommand('vscode-cue.welcome', this.cmdWelcomeCUE);
-		this.registerCommand('vscode-cue.startlsp', this.cmdStartLSP);
-		this.registerCommand('vscode-cue.stoplsp', this.cmdStopLSP);
+		this.registerCommand('welcome', this.cmdWelcomeCUE);
+		this.registerCommand('startlsp', this.cmdStartLSP);
+		this.registerCommand('stoplsp', this.cmdStopLSP);
 
 		// TODO(myitcv): in the early days of 'cue lsp', it might be worthwhile
 		// adding a command that toggles the enabled-ness of the LSP in the active
@@ -120,7 +121,8 @@ export class Extension {
 	};
 
 	// registerCommand is a light wrapper around the vscode API for registering a
-	// command but also simultaneously adding a dispose callback.
+	// command but also simultaneously adding a dispose callback. It also takes care
+	// of namespacing the command within the config.npm.name namespace.
 	//
 	// TODO(myitcv): it isn't really documented anywhere, but the expected signature
 	// of callback is:
@@ -140,12 +142,19 @@ export class Extension {
 	// the command, such that the user would otherwise be left surprised if nothing
 	// happened because of the error.
 	registerCommand = (cmd: string, callback: (context?: any) => Promise<void>) => {
+		let cmdID = this.commandID(cmd);
 		if (this.tornDown) {
 			throw errTornDown;
 		}
 
-		let disposable = vscode.commands.registerCommand(cmd, callback);
+		let disposable = vscode.commands.registerCommand(cmdID, callback);
 		this.ctx.subscriptions.push(disposable);
+	};
+
+	// commandID returns a config.npm.name-namespaced ID for cmd. e.g. with an
+	// argument of "startlsp" it will return "vscode-cue.startlsp".
+	commandID = (cmd: string): string => {
+		return config.npm.name + '.' + cmd;
 	};
 
 	// extensionConfigurationChange is the callback that fires when the extension

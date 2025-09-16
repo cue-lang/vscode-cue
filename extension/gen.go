7f5cb610bf16ac2f890af92d0a88dbbb9563de59
Jonathan Matthews <github@hello.jonathanmatthews.com>
2025-09-16 14:14:49 +0100

    extension: ensure package-lock.json version is updated
    This hack makes sure that package-lock.json's version field matches the
    field's value in package.json.
    
    A TODO tracks its replacement with a more principled approach.
    
    Change-Id: I61883fc50a22313a044696383e447cee5f82624d
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>

--- extension/gen.go
+++ extension/gen.go
@@ -2,3 +2,11 @@ package extension
 
 //go:generate go tool cue cmd genPackageJSON
 //go:generate go tool cue cmd genTS
+
+// Check that the version fields in package.json (driven by CUE) and
+// package-lock.json (potentially updated manually) have the same value.
+// TODO: replace with a CUE constraint which would permit the use of cue-vet,
+// thereby avoiding the use of -o to stop a successful cue-export's output
+// being noisily emitted to go-generate's stdout.
+//go:generate go tool cue export package.json package-lock.json -o tmp.json
+//go:generate rm tmp.json

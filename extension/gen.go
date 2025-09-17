dc4232a5fb7246663ac27a2ce66ac17d422eb804
Jonathan Matthews <github@hello.jonathanmatthews.com>
2025-09-17 08:26:44 +0100

    extension: generate package-lock.json
    CL 1222667 attempted to check that the value of the version field in
    extension/extension.cue, serialised into extension/package.json, had
    also been updated in two places in extension/package-lock.json. This
    shifted validation of package-lock.json earlier, reminding developers
    sooner if they had forgotten to update the file when bumping the
    extension's version.
    
    Unfortunately its implementation had a flaw: the "export" in
    cue-cmd-genPackageJSON suffers from cue-lang/cue#4092, preventing
    go-generate from making any progress when the extension's version gets
    bumped.
    
    This change fixes both the original shift-left problem and the newly
    introduced export problem by:
    - removing the CUE constraints added in CL 1222667, and falling back on
      CI's git-clean check to pick up misaligned version field values
    - adding an npm-install step to go-generate package-lock.json from
      package.json
    
    Change-Id: I3a3235ef3e7b8555e54eaa8a874fb17de4dfa151
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>

--- extension/gen.go
+++ extension/gen.go
@@ -2,4 +2,7 @@ package extension
 
 //go:generate go tool cue cmd genPackageJSON
 //go:generate go tool cue cmd genTS
-//go:generate go tool cue vet
+
+// Update package-lock.json from package.json.
+// TODO: consider moving inside cue-cmd-genPackageJSON.
+//go:generate npm install --silent --package-lock-only
